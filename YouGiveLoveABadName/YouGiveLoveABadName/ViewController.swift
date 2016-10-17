5ee4b8611300f0b31b139de37085e6840192f32f
mariejinpark <mariejinpark@gmail.com>
2016-10-17 10:08:47 -0400

    Bon Jovi Lab

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,15 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bonjoviImage: UIImageView!
+    
+    
+    var originalHeightConstant: CGFloat = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.bonjoviImage.translatesAutoresizingMaskIntoConstraints = false
     }
     
-    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        
-
+    func imageTapped(_ sender: UITapGestureRecognizer) {
+            UIView.animateKeyframes(withDuration: 2.0, delay: 0.0, options: [.calculationModeLinear, .autoreverse], animations: {
+            
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: (0.3), animations: {
+                   //Shrink slightly below the starting height.
+                   self.bonjoviImage.transform = CGAffineTransform(scaleX: 1.0, y: 0.75)
+                 })
+           
+                UIView.addKeyframe(withRelativeStartTime: (1/3.0), relativeDuration: 0.3, animations: {
+                    //Grow slightly beyond the height of the screen.
+                    self.bonjoviImage.transform = CGAffineTransform(scaleX: 1.0, y: 2.5)
+                })
+                
+                UIView.addKeyframe(withRelativeStartTime: (1/3.0), relativeDuration: 0.4, animations: {
+                    //the height shrinks a little
+                    self.bonjoviImage.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                })
+            })
+    
+        self.bonjoviImage.layoutIfNeeded()
     }
-
 }
+
+
+
+
+
+
+
+
+
+
 
